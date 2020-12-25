1ea3afc6cae47b34d9b3cba8352f876c3673db07
Fahmi Ariq <fahmiariq935@gmail.com>
2020-12-25 14:29:39 +0700

    menambahkan file profil.php

---
+++ profil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <h1>
+                <td>Name</td>
+                <td>:</td>
+                <td>Fahmi Production</td>
+            </h1>
+        </tr>
+    </table>
+</body>
+
+</html>
